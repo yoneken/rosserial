6c683d7fe7ef70fc1d9126e411ffe2fb213ac238
Kenta Yonekura (a.k.a yoneken) <yoneken@ieee.org>
2018-02-13 19:31:27 +0900

    [stm32] modifying codes

--- rosserial_stm32/src/ros_lib/examples/imu/Src/mainpp.cpp
+++ rosserial_stm32/src/ros_lib/examples/imu/Src/mainpp.cpp
@@ -45,13 +45,20 @@ void loop(void)
   if(!flag){
     flag = true;
 
-    //tbuf[0] = WHO_AM_I | 0x80;
-    tbuf[0] = WHO_AM_I;
+    tbuf[0] = WHO_AM_I | 0x80;
+    tbuf[1] = 0xff;
+    tbuf[2] = 0xff;
+    tbuf[3] = 0xff;
+    tbuf[4] = 0xff;
+    tbuf[5] = 0xff;
+    tbuf[6] = 0xff;
+    tbuf[7] = 0xff;
+    //tbuf[0] = ACCEL_XOUT_H | 0x80;
 
     HAL_GPIO_WritePin(GPIOA, CS_IMU_Pin, GPIO_PIN_RESET);
-    //HAL_SPI_Transmit(&hspi1, tbuf, 1, 1000);
-    //HAL_SPI_Receive(&hspi1, rbuf, 1, 1000);	// expect 0x71(113)
-    HAL_SPI_TransmitReceive(&hspi1, tbuf, rbuf, 1, 1000);
+    HAL_SPI_Transmit(&hspi1, tbuf, 1, 1000);
+    HAL_SPI_Receive(&hspi1, rbuf, 8, 1000);	// expect MPU9250-0x71(113), MPU9255-0x73(115)
+    //HAL_SPI_TransmitReceive(&hspi1, tbuf, rbuf, 8, 1000);
     HAL_GPIO_WritePin(GPIOA, CS_IMU_Pin, GPIO_PIN_SET);
 
     HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
