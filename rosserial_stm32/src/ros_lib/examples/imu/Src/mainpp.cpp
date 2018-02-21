adbf08a2e78721c44b8590276ca70a68647ba87c
Kenta Yonekura (a.k.a yoneken) <yoneken@ieee.org>
2018-02-21 15:42:28 +0900

    use smart_ptr

--- rosserial_stm32/src/ros_lib/examples/imu/Src/mainpp.cpp
+++ rosserial_stm32/src/ros_lib/examples/imu/Src/mainpp.cpp
@@ -10,6 +10,7 @@
 #include "mpu9250.h"
 #include <mainpp.h>
 #include <queue>
+#include <memory>
 #include <ros.h>
 #include <std_msgs/String.h>
 #include "sensor_msgs/Imu.h"
@@ -30,30 +31,36 @@ class SPI_Mem{
 public:
   unsigned short int chip;
   short data_num;
-  uint8_t *data;
+  std::shared_ptr<uint8_t>data;
   void (*callback)(uint8_t dat[]);
 
-  SPI_Mem(unsigned short int chip_, uint8_t address_, void (*callback_)(uint8_t dat[]), short num_, const uint8_t data_[])
-    : chip(chip_), data_num(num_), data(new uint8_t[1+num_]), callback(callback_)
+  // send message
+  SPI_Mem(unsigned short int chip_, uint8_t address_, short num_, const uint8_t data_[])
+    : chip(chip_), data_num(num_), data(std::make_shared<uint8_t>(1+num_)), callback(NULL)
   {
-    data[0] = address_;
-    if(data_ != NULL){
-      memcpy(&(data[1]), data_, num_);
-    }else{
-      memset(&(data[1]), 0xff, num_+1);
-    }
+    uint8_t *dat = data.get();
+    dat[0] = address_;
+    memcpy(&(dat[1]), data_, num_);
   }
-  virtual ~SPI_Mem(){ delete[] data; }
+
+  // receive message
+  SPI_Mem(unsigned short int chip_, uint8_t address_, void (*callback_)(uint8_t dat[]), short num_)
+    : chip(chip_), data_num(num_), data(std::make_shared<uint8_t>(1+num_)), callback(callback_)
+  {
+    uint8_t *dat = data.get();
+    dat[0] = address_;
+    memset(&(dat[1]), 0xff, num_+1);
+  }
+
+  virtual ~SPI_Mem(){ }
   SPI_Mem(const SPI_Mem& mem)
-    : chip(mem.chip), data_num(mem.data_num), data(new uint8_t[1+mem.data_num]), callback(mem.callback)
-  { memcpy(data, mem.data, data_num + 1); }
+    : chip(mem.chip), data_num(mem.data_num), data(std::make_shared<uint8_t>(1+mem.data_num)), callback(mem.callback)
+  { data = mem.data; }
 
   SPI_Mem& Copy(const SPI_Mem& mem)
   {
     if(this == &mem){ return *this; }
-    delete[] data;
-    data = new uint8_t[1+mem.data_num];
-    memcpy((void *)data, mem.data, 1+mem.data_num);
+    data = mem.data;
     return *this;
   }
 
@@ -61,33 +68,35 @@ public:
 };
 
 volatile bool flag_transmit = false;
-std::queue<SPI_Mem>spiq;
+std::queue<std::shared_ptr<SPI_Mem>>spiq;
 uint8_t rbuf[64];
 
 
 void SPI_Mem_Transmit(void){
+  HAL_SPI_StateTypeDef state = HAL_SPI_GetState(&hspi1);
   if(!flag_transmit){
     flag_transmit = true;
-    SPI_Mem m = spiq.front();
-    HAL_GPIO_WritePin(GPIOA, m.chip, GPIO_PIN_RESET);
-    HAL_SPI_TransmitReceive_DMA(&hspi1, m.data, rbuf, 1+m.data_num);
+    SPI_Mem *m = spiq.front().get();
+    HAL_GPIO_WritePin(GPIOA, m->chip, GPIO_PIN_RESET);
+    HAL_SPI_TransmitReceive_DMA(&hspi1, m->data.get(), rbuf, 1+m->data_num);
   }
 }
 
 void SPI_Mem_Write(unsigned short int chip, uint8_t address, short num, const uint8_t data[]){
-  spiq.push(SPI_Mem(chip, address, NULL, num, data));
+  spiq.push(std::make_shared<SPI_Mem>(chip, address, num, data));
   SPI_Mem_Transmit();
 }
 
 void SPI_Mem_Read(unsigned short int chip, uint8_t address, void (*callback)(uint8_t dat[]), short num){
-  spiq.push(SPI_Mem(chip, address | 0x80, callback, num, NULL));
+  spiq.push(std::make_shared<SPI_Mem>(chip, address | 0x80, callback, num));
   SPI_Mem_Transmit();
 }
 
 void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi){
-  SPI_Mem m = spiq.front();
-  HAL_GPIO_WritePin(GPIOA, m.chip, GPIO_PIN_SET);
-  if(m.callback != NULL) m.callback(&(rbuf[1]));
+  SPI_Mem *m = spiq.front().get();
+  HAL_GPIO_WritePin(GPIOA, m->chip, GPIO_PIN_SET);
+  if(m->callback != NULL) m->callback(&(rbuf[1]));
+  uint8_t s = spiq.size();
   spiq.pop();
   flag_transmit = false;
   if(!spiq.empty()) SPI_Mem_Transmit();
@@ -125,40 +134,40 @@ void setup(void)
   uctr[0].I2C_MST_EN = 1;	// Enable the I2C Master I/F module
   uctr[0].I2C_IF_DIS = 1;	// Disable I2C Slave module and put the serial interface in SPI mode only
   //uctr[0].BYTE = 0x30;
-  SPI_Mem_Write(CS_IMU_Pin, USER_CTRL, 1, (uint8_t*)uctr);
+  SPI_Mem_Write(CS_IMU_Pin, USER_CTRL, 1, (uint8_t*)uctr);	// expect 0x30(48)
 
   I2C_MST_CTRL_REG i2cmctr[1] = {0};
   i2cmctr[0].I2C_MST_CLK = _400kHz;	// Set I2c Master clock to 400kHz
-  SPI_Mem_Write(CS_IMU_Pin, I2C_MST_CTRL, 1, (uint8_t*)i2cmctr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_MST_CTRL, 1, (uint8_t*)i2cmctr);		// expect 0x0D(13)
 
   // Reset AK8963 (Magnetometer implemented on mpu9250) through I2C
   I2C_SLV0_ADDR_REG i2caddr[1] = {0};
   i2caddr[0].I2C_ID_0 = 0x0c;	// Since AK8963 is addressed on 0x0c
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_ADDR, 1, (uint8_t*)i2caddr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_ADDR, 1, (uint8_t*)i2caddr);	// expect 0x0C(12)
 
   I2C_SLV0_REG_REG i2creg[1] = {0};
   i2creg[0].BYTE = CNTL2;
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);	// expect 0x0B(11)
 
   AK8963_CNTL2_REG cntl2[1] = {0};
   cntl2[0].SRST = 1;	// Enable reset
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_DO, 1, (uint8_t*)cntl2);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_DO, 1, (uint8_t*)cntl2);	// expect 0x01(1)
 
   I2C_SLV0_CTRL_REG i2cctr[1] = {0};
   i2cctr[0].I2C_SLV0_EN = 1;	// Eneble I2C
   i2cctr[0].I2C_SLV0_LENG = 1;	// Write 1 byte
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);	// expect 0x81(129)
 
   // Initialize AK8963 for continuous sensing mode through I2C
   i2creg[0].BYTE = CNTL;
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);	// expect 0x0A(10)
 
   AK8963_CNTL1_REG cntl1[1] = {0};
   cntl1[0].MODE = CONT_MES_MODE2;	// continuous measuring mode
   cntl1[0].BIT = OUT_16BIT;			// measure 16 bit
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_DO, 1, (uint8_t*)cntl1);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_DO, 1, (uint8_t*)cntl1);	// expect 0x16(22)
 
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);	// expect 0x81(129)
 
 }
 
@@ -171,16 +180,16 @@ void loop(void)
   I2C_SLV0_ADDR_REG i2caddr[1] = {0};
   i2caddr[0].I2C_ID_0 = 0x0c;	// Since AK8963 is addressed on 0x0c
   i2caddr[0].I2C_SLV0_RNW = 1;	// read flag
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_ADDR, 1, (uint8_t*)i2caddr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_ADDR, 1, (uint8_t*)i2caddr);	// expect 0x8C(140)
 
   I2C_SLV0_REG_REG i2creg[1] = {0};
   i2creg[0].BYTE = HXL;
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_REG, 1, (uint8_t*)i2creg);	// expect 0x03(3)
 
   I2C_SLV0_CTRL_REG i2cctr[1] = {0};
   i2cctr[0].I2C_SLV0_EN = 1;	// Eneble I2C
   i2cctr[0].I2C_SLV0_LENG = 7;	// Read 7 byte from HXL to ST2
-  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);
+  SPI_Mem_Write(CS_IMU_Pin, I2C_SLV0_CTRL, 1, (uint8_t*)i2cctr);	// expect 0x87(135)
 
   SPI_Mem_Read(CS_IMU_Pin, EXT_SENS_DATA, cb_mag_sense, 7);
   /*
